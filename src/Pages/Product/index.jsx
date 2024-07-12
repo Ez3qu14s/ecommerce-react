c8d5614325dae516cd0d3d284efa4bc2c7e1fe14
Ezequias Bernardo <62806474+Ez3qu14s@users.noreply.github.com>
2024-07-12 13:30:06 -0300

    feat: finalizar pagina de produto

--- src/Pages/Product/index.jsx
+++ src/Pages/Product/index.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
-import { useParams } from 'react-router-dom';
+import { Link, useParams } from 'react-router-dom';
 import api from '../../api';
+import './style.css';
 
 const Product = () => {
   const [produto, setProdutos] = React.useState(null);
@@ -12,11 +13,40 @@ const Product = () => {
     });
   }, []);
 
-  console.log(produto);
   return (
-    <div>
-      <h1>Hello world</h1>
-    </div>
+    <>
+      <Link to="/">
+        <h1 className="before:content-['<'] before:mr-2 text-lg font-semibold text-gray-400">
+          Produtos
+        </h1>
+      </Link>
+
+      {produto ? (
+        <div className="grid gap-6 md:gap-16 lg:gap-32">
+          <img
+            className="md:w-[300px] lg:w-[340px] justify-self-center w-[200px]"
+            src={produto.image}
+            alt=""
+          />
+          <div className="content-center">
+            <h1 className="text-xl font-semibold">{produto.title}</h1>
+            <span className="text-[#6b7280] font-light">
+              {produto.category}
+            </span>
+            <p className="my-4">{produto.description}</p>
+
+            <p className="text-3xl font-semibold">R$ {produto.price}</p>
+            <button className="mt-6 lg:w-[200px] w-full py-2 bg-[#0369A1] text-[#f0f9ff] font-medium">
+              Comprar
+            </button>
+          </div>
+        </div>
+      ) : (
+        <div className="w-full mt-6">
+          <p className="text-2xl font-semibold">Loading...</p>
+        </div>
+      )}
+    </>
   );
 };
 
